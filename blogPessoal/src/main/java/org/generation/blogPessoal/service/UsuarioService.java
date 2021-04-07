6c8da71a5c1f3b5281e1728c219aa669ccdf84c2
unknown <fabiolimadiogenes@gmail.com>
2021-04-07 08:07:17 -0300

    Atividade implementando as camadas de segurança no Back-end de BlogPessoal.

---
+++ blogPessoal/src/main/java/org/generation/blogPessoal/service/UsuarioService.java
@@ -0,0 +1,51 @@
+package org.generation.blogPessoal.service;
+
+import java.nio.charset.Charset;
+import java.util.Optional;
+
+import javax.print.DocFlavor.BYTE_ARRAY;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.generation.blogPessoal.model.UserLogin;
+import org.generation.blogPessoal.model.Usuario;
+import org.generation.blogPessoal.repository.usuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+	@Autowired
+	private usuarioRepository repository;
+	
+	public Usuario CadastraUsuario(Usuario usuario) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String senhaEncoder = encoder.encode(usuario.getSenha());
+		usuario.setSenha(senhaEncoder);
+		
+		return repository.save(usuario);
+	}
+	
+	public Optional<UserLogin> Logar(Optional<UserLogin> user){
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		Optional<Usuario> usuario = repository.findByUsuario(user.get().getUsuario());
+		
+		if(usuario.isPresent()){
+			if(encoder.matches(user.get().getSenha(), usuario.get().getSenha())); { //verifica se a senha encripitada e a não encripitada são iguais
+				
+				String auth = user.get().getUsuario() + ":" + user.get().getSenha();
+				byte[] encodeAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")));
+				String authHeader = "Basic " + new String(encodeAuth);
+				
+				user.get().setToken(authHeader);
+				user.get().setNome(usuario.get().getNome());
+				user.get().setSenha(usuario.get().getSenha());
+				
+				return user;
+				
+			}	
+		}
+		return null;
+	}
+}	
